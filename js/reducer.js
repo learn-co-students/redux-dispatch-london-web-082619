0288f4f49039d3bce566bcb44429a36dde9ffd0e
Abdullah Bajaman <ambajaman@gmail.com>
2019-11-29 01:25:24 +0000

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -10,4 +10,14 @@ function changeState(state, action){
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+const dispatch = (action) => {
+  state = changeState(state, action)
+  render()
+}
+
+const render = () => {
+  document.body.textContent = state.count
+}
+// changeState(state, action)
+
+render()
